801337f381b7ea3f0540f8c89786ea0c6237f28f
pbckman <143405511+pbckman@users.noreply.github.com>
2023-12-07 14:52:28 +0100

    img option chooser for patiocalculator added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function calculateCost() {
     var length = document.getElementById('length').value;
     var width = document.getElementById('width').value;
-    var material = document.getElementById('material').value;
+    var material = document.getElementById('selectedMaterial').value;
 
     var area = length * width;
     var cost = calculateMaterialCost(area, material);
@@ -10,15 +10,21 @@ function calculateCost() {
 }
 
 function calculateMaterialCost(area, material) {
-    
+
     var costPerSquareMeter = 100;
 
     switch (material) {
-        case 'trä':
+        case 'trä1':
             costPerSquareMeter = 150;
             break;
-        case 'trä1':
+        case 'trä2':
             costPerSquareMeter = 200;
+            break;
+        case 'trä3':
+            costPerSquareMeter = 300;
+            break;
+        case 'trä4':
+            costPerSquareMeter = 400;
             break;
     }
 
@@ -31,6 +37,7 @@ function calculateCostL() {
     var widthA = document.getElementById('widthA').value;
     var lengthB = document.getElementById('lengthB').value;
     var widthB = document.getElementById('widthB').value;
+    var materialL = document.getElementById('selectedMaterial').value;
 
     var areaL = lengthA * widthA + lengthB * widthB;
     var costL = calculateMaterialCostL(areaL, materialL)
@@ -39,17 +46,27 @@ function calculateCostL() {
 }
 
 function calculateMaterialCostL(areaL, materialL) {
-    
+
     var costPerSquareMeter = 100;
 
     switch (materialL) {
-        case 'trä':
+        case 'trä1':
             costPerSquareMeter = 150;
             break;
-        case 'trä1':
+        case 'trä2':
             costPerSquareMeter = 200;
+            break;
+        case 'trä3':
+            costPerSquareMeter = 300;
+            break;
+        case 'trä4':
+            costPerSquareMeter = 400;
             break;
     }
 
     return areaL * costPerSquareMeter;
+}
+
+function selectMaterial(material) {
+    document.getElementById('selectedMaterial').value = material;
 }
