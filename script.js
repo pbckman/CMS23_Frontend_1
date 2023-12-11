670c0f63b6ea8bbdf2490ff4530df6251240cf4d
pbckman <143405511+pbckman@users.noreply.github.com>
2023-12-11 15:24:52 +0100

    updated patioCalculator

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-function calculateCost() {
-    var length = document.getElementById('length').value;
-    var width = document.getElementById('width').value;
-    var material = document.getElementById('selectedMaterial').value;
-
-    var area = length * width;
-    var cost = calculateMaterialCost(area, material);
-
-    document.getElementById('result').innerHTML = 'Kostnad: ' + cost + ' kr';
-}
-
 function calculateMaterialCost(area, material) {
-
     var costPerSquareMeter = 100;
 
     switch (material) {
@@ -31,42 +19,101 @@ function calculateMaterialCost(area, material) {
     return area * costPerSquareMeter;
 }
 
+function calculateCost() {
+    var material = document.getElementById('selectedMaterial').value;
+
+    if (document.getElementById('rektInputs').style.display === 'block') {
+    var length = document.getElementById('length').value;
+    var width = document.getElementById('width').value;
+    
+
+    var area = length * width;
+    var cost = calculateMaterialCost(area, material);
+
+    document.getElementById('result').innerHTML = 'Kostnad: ' + cost + ' kr';
+    }
+}
 
 function calculateCostL() {
+    var materialL = document.getElementById('selectedMaterial').value;
+
+    if (document.getElementById('LInputs').style.display === 'block') {
     var lengthA = document.getElementById('lengthA').value;
     var widthA = document.getElementById('widthA').value;
     var lengthB = document.getElementById('lengthB').value;
     var widthB = document.getElementById('widthB').value;
-    var materialL = document.getElementById('selectedMaterial').value;
+    
 
     var areaL = lengthA * widthA + lengthB * widthB;
-    var costL = calculateMaterialCostL(areaL, materialL)
+    var costL = calculateMaterialCost(areaL, materialL);
 
     document.getElementById('resultL').innerHTML = 'Kostnad: ' + costL + ' kr';
+    }
 }
 
-function calculateMaterialCostL(areaL, materialL) {
 
-    var costPerSquareMeter = 100;
 
-    switch (materialL) {
-        case 'trä1':
-            costPerSquareMeter = 150;
-            break;
-        case 'trä2':
-            costPerSquareMeter = 200;
-            break;
-        case 'trä3':
-            costPerSquareMeter = 300;
-            break;
-        case 'trä4':
-            costPerSquareMeter = 400;
-            break;
+function selectShape(shape) {
+    var rektInputs = document.getElementById('rektInputs');
+    var LInputs = document.getElementById('LInputs');
+    var calculateButton = document.getElementById('calculateButton');
+    var calculateButtonL = document.getElementById('calculateButtonL');
+
+    if (shape === 'trall1') {
+        rektInputs.style.display = 'block';
+        LInputs.style.display = 'none';
+        calculateButton.style.display = 'block';
+        calculateButtonL.style.display = 'none';
+    } else if (shape === 'trall2') {
+        rektInputs.style.display = 'none';
+        LInputs.style.display = 'block';
+        calculateButton.style.display = 'none';
+        calculateButtonL.style.display = 'block';
     }
+}
 
-    return areaL * costPerSquareMeter;
+
+function selectMaterial(material) {
+  document.getElementById('selectedMaterial').value = material;
+}
+
+
+
+function selectShapeAddBorder(shape) {
+    // Ta bort klassen från alla bilder
+    var allImages = document.querySelectorAll('.img-shape-option');
+    allImages.forEach(function(img) {
+        img.classList.remove('selected');
+    });
+
+    // Lägg till klassen på den valda bilden
+    var selectedImage = document.getElementById('img-' + shape);
+    selectedImage.classList.add('selected');
 }
 
 function selectMaterial(material) {
+    // För rektangulär altan
+    var allMaterialImagesRectangular = document.querySelectorAll('#materialImagesRectangular .img-material-option');
+    allMaterialImagesRectangular.forEach(function(img) {
+        img.classList.remove('selected');
+    });
+
+    var selectedMaterialImageRectangular = document.querySelector('#materialImagesRectangular img[alt="' + material + '"]');
+    if (selectedMaterialImageRectangular) {
+        selectedMaterialImageRectangular.classList.add('selected');
+    }
+
+    // För L-formad altan
+    var allMaterialImagesL = document.querySelectorAll('#materialImagesL .img-material-option');
+    allMaterialImagesL.forEach(function(img) {
+        img.classList.remove('selected');
+    });
+
+    var selectedMaterialImageL = document.querySelector('#materialImagesL img[alt="' + material + '"]');
+    if (selectedMaterialImageL) {
+        selectedMaterialImageL.classList.add('selected');
+    }
+
+    // Uppdatera det gömda fältet med valt material
     document.getElementById('selectedMaterial').value = material;
 }
